2aa6af8701babe3aaa6e9e2d0c2aee1c4dcdfab5
Tonlito <Antoniow4@hotmail.com>
2025-11-05 14:40:01 +0100

    Added AboutCallbackSection with Rte and form. Form added and using Service,Controller and viewmodel to handle requests. Fixed css problems with nesting and etc. Js for keeping form in center.

---
+++ Controllers/FormController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Web.Website.Controllers;
+using Umbraco_Assignment_CMS.Services;
+using Umbraco_Assignment_CMS.ViewModels;
+
+namespace Umbraco_Assignment_CMS.Controllers;
+
+public class FormController(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory, ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider, FormSubmissonsService formSubmissonsService) : SurfaceController(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
+{
+    private readonly FormSubmissonsService _formSubmissonsService = formSubmissonsService;
+
+    public IActionResult HandleCallbackFOrm(CallbackFormViewModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return CurrentUmbracoPage();
+        }
+        var result = _formSubmissonsService.SaveCallbackRequest(model);
+
+        if(!result)
+        {
+            // FormError
+            TempData["FormError"] = "Something went wrong while submitting you request. Please try again later.";
+            return RedirectToCurrentUmbracoPage();
+        }
+
+        // FormSuccess
+        TempData["FormSuccess"] = "Thank you! Your Request have been noted. Thank you!";
+        return RedirectToCurrentUmbracoPage();
+    }
+}
